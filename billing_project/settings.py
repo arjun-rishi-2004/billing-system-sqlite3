de60c5e791a4428c3334f51841c9596e69b96470
Arjun Rishi <128972815+arjun-rishi-2004@users.noreply.github.com>
2023-10-19 09:07:05 +0530

    Update settings.py postgres

--- billing_project/settings.py
+++ billing_project/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'billing_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "billing-system-postgres",
+        "USER": "default",
+        "PASSWORD": "0acgQw3OzWlr",
+        "HOST": "ep-noisy-resonance-76492460-pooler.us-east-1.postgres.vercel-storage.com",
+
+        "PORT": "5432",
     }
 }
 
